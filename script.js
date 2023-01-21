1d3ab7f5f6559590fa87ef1017e031cab94e7745
Wuller Barros <wuller1987@gmail.com>
2023-01-21 11:20:54 -0300

    enviando tarefa

---
+++ script.js
@@ -0,0 +1,51 @@
+    //QUESTÃO 0
+
+    var total = 10 + 5 * 2 / 2 + 20
+    console.log(total)
+
+    //QUESTÃO 1
+
+    var frase = "Isso é uma frase"
+    var numero = 10
+
+    resultado = frase / numero
+    console.log(resultado)
+    resultado2 = frase * numero
+    console.log(resultado2)
+
+    //QUESTÃO 2
+
+    var n1 = "90"
+    var n2 = 10
+
+    var r1 = parseInt(n1) + n2
+    console.log(r1)
+
+    //QUESTÃO 3 
+
+    var incremento = 10
+    console.log(++incremento)
+
+    //QUESTÃO 4
+
+    var numero = '80'
+    var unidade = 'kg'
+    var peso = numero + unidade
+
+    console.log(peso)
+
+    var pesoPorDois = parseInt(numero)/2
+    pesoPorDois = pesoPorDois+unidade
+    console.log(pesoPorDois)
+
+    //QUESTÃO 5
+
+    var idade = prompt("DIGITA A IDADE AE COLEGA")
+    if(idade>=18){
+        alert("MAIOR DE IDADE")
+    }
+    else{
+        alert("MENOR DE IDADE")
+    }
+
+
